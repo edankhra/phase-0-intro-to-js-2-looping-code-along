b13f0b4a308802f5234cb02efa292fb5cbf300e1
Ekta <edankhra@gmail.com>
2023-03-12 22:09:36 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function countDown() {
+  let num =10;
+    while (num >= 0) {
+      console.log(num);
+      num--;
+    }
+  }
+  function writeCards(name, eventsName) {
+    const names = ["Guadalupe", "Ollie", "Aki"];
+    const eventName =  "surprise";
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return messages;
+  }
